2cf72c1442f658c793759af87a773891ca287d31
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-05 22:03:23 +0530

    Update load_cifar10.py

--- Assignment6/load_cifar10.py
+++ Assignment6/load_cifar10.py
@@ -2,6 +2,7 @@ import albumentations as A
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import cv2 
 
 augmentations = [A.HorizontalFlip(), A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15),
                  A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16,
