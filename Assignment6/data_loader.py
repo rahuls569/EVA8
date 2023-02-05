f61809d66fc35124df1ba65ec92da86626ded7a5
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-05 19:06:09 +0530

    Update data_loader.py

--- Assignment6/data_loader.py
+++ Assignment6/data_loader.py
@@ -1,25 +1,27 @@
+import albumentations as A
 import torch
-
 import torchvision
-from torchvision import datasets, transforms
-import os
-import cv2
-from albumentations import Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout
-from albumentations.pytorch.transforms import ToTensorV2
+import torchvision.transforms as transforms
 
-
-def load_cifar10(root, download=True):
+def load_cifar10(root='./data', augmentations=None):
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     trainset = torchvision.datasets.CIFAR10(root=root, train=True,
-                                            download=download, transform=transform)
+                                            download=True, transform=transform)
 
     testset = torchvision.datasets.CIFAR10(root=root, train=False,
-                                           download=download, transform=transform)
+                                           download=True, transform=transform)
 
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     
+    if augmentations is not None:
+        augmentations = A.Compose(augmentations)
+        
+        trainset.train_data = np.array([augmentations(image=img)["image"] for img in trainset.train_data])
+        trainset.train_data = trainset.train_data.astype(np.float32)
+        trainset.train_data = torch.from_numpy(trainset.train_data)
+
     return trainset, testset, classes
