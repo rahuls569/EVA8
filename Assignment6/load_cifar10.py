21c661ea13bcd600a194e1c6d136db73f852f5a8
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-05 20:15:58 +0530

    Update load_cifar10.py

--- Assignment6/load_cifar10.py
+++ Assignment6/load_cifar10.py
@@ -24,15 +24,4 @@ def load_cifar10(root, augmentations=None):
     
     return trainset, testset, classes
  
-def setup_dataloaders(trainset, testset, SEED, Batch_size):
-    cuda = torch.cuda.is_available()
-    torch.manual_seed(SEED)
-    if cuda:
-        torch.cuda.manual_seed(SEED)
-    
-    dataloader_args = dict(shuffle=True, batch_size=Batch_size, num_workers=2, pin_memory=True) if cuda else dict(shuffle=True, batch_size=Batch_size)
-    
-    train_loader = torch.utils.data.DataLoader(trainset, **dataloader_args)
-    test_loader = torch.utils.data.DataLoader(testset, **dataloader_args)
-    
-    return train_loader, test_loader
+
