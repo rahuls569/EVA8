d2a879d9c5bc68dfffd5c7f6d75db5e8e745ed6d
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-05 22:29:16 +0530

    Update load_cifar10.py

--- Assignment6/load_cifar10.py
+++ Assignment6/load_cifar10.py
@@ -8,23 +8,35 @@ augmentations = [A.HorizontalFlip(), A.ShiftScaleRotate(shift_limit=0.0625, scal
                  A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16,
                                  min_width=16, fill_value=(0.5, 0.5, 0.5), mask_fill_value=None)]
 
+def augment(image, transform):
+    image = transforms.ToPILImage()(image)
+    for aug in augmentations:
+        image = aug(image=image)["image"]
+    return transform(image)
+
 def load_cifar10(root, augmentations=None):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+   
+    train_transforms = transforms.Compose([
+                                      #  transforms.Resize((28, 28)),
+                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                     
+                                       transforms.ToTensor(),
+                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
+                                       # Note the difference between (0.1307) and (0.1307,)
+                                       ])
+    test_transforms = transforms.Compose([
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
+                                       ])
     
-    trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=transform1)
+    trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=train_transforms)
     
     if augmentations is not None:
-        def augment(image):
-            image = transforms.ToPILImage()(image)
-            for aug in augmentations:
-                image = aug(image=image)["image"]
-            return transform(image)
-        transform1 = augment
-        
-    testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=transform)
+        transform1 = lambda image: augment(image, train_transforms)
+    else:
+        transform1 = train_transforms
+    
+    testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=test_transforms)
     
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     
