26994cabb52e931c4b97fc13749d16d26cb69196
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-05 18:59:36 +0530

    Update data_loader.py

--- Assignment6/data_loader.py
+++ Assignment6/data_loader.py
@@ -7,8 +7,19 @@ import cv2
 from albumentations import Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout
 from albumentations.pytorch.transforms import ToTensorV2
 
-def transform1():
-    return transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+def load_cifar10(root, download=True):
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root=root, train=True,
+                                            download=download, transform=transform)
+
+    testset = torchvision.datasets.CIFAR10(root=root, train=False,
+                                           download=download, transform=transform)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    
+    return trainset, testset, classes
