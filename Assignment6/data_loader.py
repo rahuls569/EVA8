b706b3aa72a7851a73216349bdea0fce9ffc4c18
rahuls569 <79099957+rahuls569@users.noreply.github.com>
2023-02-05 18:51:48 +0530

    Update data_loader.py

--- Assignment6/data_loader.py
+++ Assignment6/data_loader.py
@@ -1,23 +1,14 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+
 import torchvision
 from torchvision import datasets, transforms
-
+import os
+import cv2
+from albumentations import Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout
+from albumentations.pytorch.transforms import ToTensorV2
 
 def transform1():
-  transform = transforms.Compose(
-      [transforms.ToTensor(),
-       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    return transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-  trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                          download=True, transform=transform)
-
-
-  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                         download=True, transform=transform)
-
-
-  classes = ('plane', 'car', 'bird', 'cat',
-             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
